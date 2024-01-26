ae54ab60504d872544d19d2c59c965ac216c39f3
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2024-01-26 11:48:55 +0300

    Time: 71 ms (28.04%) | Memory: 11.6 MB (97.58%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,28 +1,17 @@
 
 class Solution(object):
     def isValidSudoku(self, board):
-        #row by row
+        track=set()
         for i in range(9):
-            countRow = {str(k):0 for k in range(0,10)}
-            countCol = {str(k):0 for k in range(0,10)}
             for j in range(9):
-                currRow=board[i][j]
-                if currRow != '.': 
-                    countRow[currRow]+=1
-                    if countRow[currRow] > 1: return False
-                currCol=board[j][i]
-                if currCol != '.': 
-                    countCol[currCol]+=1
-                    if countCol[currCol] > 1: return False
-        
-        #bloc by bloc
-        for bi in range(0, 9, 3):
-            for bj in range(0, 9, 3):
-                count = {str(k):0 for k in range(0,10)}
-                for i in range(bi, bi+3):
-                    for j in range(bj, bj+3):
-                        curr=board[i][j]
-                        if curr != '.': 
-                            count[curr]+=1
-                            if count[curr] > 1: return False
+                curr=board[i][j]
+                if curr !=".":
+                    rowStr = curr+" in row "+str(i)
+                    colStr = curr+" in col "+str(j)
+                    blockStr = curr+" in block "+str((i//3)*3)+"-"+str((j//3)*3)
+                    if rowStr in track or colStr in track or blockStr in track: return False
+                    track.add(rowStr)
+                    track.add(colStr)
+                    track.add(blockStr)
+                
         return True        
