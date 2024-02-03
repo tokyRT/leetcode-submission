199fce6adeea929427971e427f05265bd79a224c
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2024-02-03 11:23:22 +0300

    Time: 54 ms (5.19%) | Memory: 11.7 MB (90.58%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,14 @@
 class Solution(object):
     def isValid(self, s):
-        stack=[]
-        lefts={
-            ")": "(",
-            "]": "[",
-            "}": "{"
-        }
-        for elem in s:
-            if len(stack)==0:
-                stack.append(elem)
-            else:
-                if elem in ")]}" and stack[-1]==lefts[elem]:
-                    stack.pop()
-                else:
-                    stack.append(elem)
-
-        return True if len(stack)==0 else False
+        v=s
+        while True:
+            if v=="": return True
+            tmp=v
+            print(v)
+            v=v.replace("()", "")
+            v=v.replace("[]", "") 
+            v=v.replace("{}", "")
+            print(v)
+            if v==tmp: return False
+        return True
         
