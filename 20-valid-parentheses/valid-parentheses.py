e9efc6cdeec56a69e3f578457547c9958b0576a2
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2024-02-03 11:24:36 +0300

    Time: 53 ms (5.19%) | Memory: 11.8 MB (83.52%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,12 +3,11 @@ class Solution(object):
         v=s
         while True:
             if v=="": return True
+
             tmp=v
-            print(v)
             v=v.replace("()", "")
             v=v.replace("[]", "") 
             v=v.replace("{}", "")
-            print(v)
+            
             if v==tmp: return False
-        return True
         
