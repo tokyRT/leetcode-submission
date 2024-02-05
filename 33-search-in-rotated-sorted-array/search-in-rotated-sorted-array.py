6110772e5fbab14a5502d52b6673d764b25f053f
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2024-02-05 16:35:20 +0300

    Time: 19 ms (84.67%) | Memory: 12 MB (76.59%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,39 @@
+def pivot(nums):
+    left, right = 0, len(nums)-1
+    ans=nums[0]
+    pivot=0
+    while left <= right:
+        mid = (left+right)//2
+        if nums[left] <= nums[mid]:
+            if nums[left] < ans: pivot=left
+            ans = min(ans, nums[left])
+            left  = mid+1
+        else:
+            if nums[mid] < ans: pivot=mid
+            ans=min(ans, nums[mid])
+            right=mid-1
+    return pivot
+class Solution(object):
+    def search(self, nums, target):
+        if len(nums)==1:
+            return 0 if nums[0]==target else -1
+        p=pivot(nums)
+        ans=-1
+        if p > 0 and (nums[0] <= target <= nums[p-1]):
+            left=0
+            right=p-1
+        else:
+            left=p
+            right=len(nums)-1
+        print(left, right)
+        while left <= right:
+            mid=(left+right)//2
+            if target == nums[mid]:
+                ans=mid
+                break
+            elif target<nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
+        
