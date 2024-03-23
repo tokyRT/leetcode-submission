8e74eb958a098357bd9f6a509d9e93d9bfd6f401
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2024-03-23 10:56:36 +0300

    Time: 12 ms (84.33%) | Memory: 11.7 MB (41.10%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,13 +1,14 @@
 import math
+from collections import Counter
 def combinaisons(n, k):
   return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
 class Solution(object):
     def numIdenticalPairs(self, nums):
-        c={}
-        for i in nums:
-            if i in c.keys():
-                c[i]+=1
-            else: c[i]=1
+        c=Counter(nums)
+        # for i in nums:
+        #     if i in c.keys():
+        #         c[i]+=1
+        #     else: c[i]=1
         r=0
         for k, v in c.items():
             if v>1: r+=combinaisons(v,2)
