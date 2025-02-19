4b9c494ac0f154b576ed63e204764fb97a4a3692
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2025-02-19 18:02:22 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.26%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,20 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        retenu = 1
-        res = []
-        i=len(digits)-1
-        while i >= 0:
-            tmp = digits[i] + 1 if retenu == 1 else digits[i]
-            res.insert(0, tmp % 10)
-            if tmp == 10: retenu = 1
-            else: retenu = 0
+        n = int("".join([str(i) for i in digits])) + 1
+        res = str(n)
 
-            i-=1
-
-        if retenu == 1:
-            res.insert(0, 1)
-            retenu = 0
-
-        return res
+        return [int(i) for i in res]
         
