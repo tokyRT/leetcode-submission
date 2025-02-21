3f2aed52a0f86cf59745b7f954a0339542b60043
Toky RATOLOJANAHARY <tokyrt.pro@gmail.com>
2025-02-21 23:23:26 +0300

    Time: 13 ms (72.34%) | Memory: 13.9 MB (42.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,13 @@
-import math
 class Solution(object):
     def isPalindrome(self, s):
-        s="".join(l.lower() for l in s if l.isalnum())
-        print(s)
-        right=len(s)-1
-        for left in range(int(math.ceil(len(s)/2))):
-            if s[left] != s[right]: return False
-            right-=1
+        s=s.lower()
+        s="".join([i for i in s if i.isalnum()])
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
         return True
         
